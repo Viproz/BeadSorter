105db71f1327a5dc6749f83e2753254e9393233a
Viproz <viproz12@gmail.com>
2022-08-21 20:15:41 +0200

    Added batch script.

---
+++ STLs/ExtractParts.bat
@@ -0,0 +1,30 @@
+REM LinusB parts
+xcopy LinusB\0-hopper-seq-stopper.stl           TO_PRINT\.
+xcopy LinusB\1-sequentializer-ilc-bottom.stl    TO_PRINT\.
+xcopy LinusB\1-sequentializer-ilc-top.stl       TO_PRINT\.
+xcopy LinusB\1-sequentializer-motor-holder.stl  TO_PRINT\.
+xcopy LinusB\3-analyzer-base.stl                TO_PRINT\.
+xcopy LinusB\3-analyzer-exit-base.stl           TO_PRINT\.
+xcopy LinusB\3-analyzer-exit-tube.stl           TO_PRINT\.
+xcopy LinusB\3-analyzer-ilc-bottom.stl          TO_PRINT\.
+xcopy LinusB\3-analyzer-ilc-corner.stl          TO_PRINT\.
+xcopy LinusB\3-analyzer-ilc-top.stl             TO_PRINT\.
+xcopy LinusB\3-analyzer-separation-arm.stl      TO_PRINT\.
+xcopy LinusB\3-analyzer-servo-holder.stl        TO_PRINT\.
+xcopy LinusB\3-analyzer-top-alignment.stl       TO_PRINT\.
+xcopy LinusB\4-dispatcher-ilc-bottom.stl        TO_PRINT\.
+xcopy LinusB\4-dispatcher-ilc-corner.stl        TO_PRINT\.
+xcopy LinusB\4-dispatcher-ilc-top.stl           TO_PRINT\.
+xcopy LinusB\4-dispatcher-tube-alignment.stl    TO_PRINT\.
+xcopy LinusB\4-dispatcher-tubehalf-1.stl        TO_PRINT\.
+xcopy LinusB\4-dispatcher-tubehalf-2.stl        TO_PRINT\.
+xcopy LinusB\5-outputs-ilc-corner.stl           TO_PRINT\.
+xcopy LinusB\5-outputs-ilc-top.stl              TO_PRINT\.
+
+REM RichA777 parts
+xcopy RichA777\0-hopper-seq-plate-4_holes_modified.stl TO_PRINT\.
+xcopy RichA777\Hopper_Modified_Cleaned_up.stl          TO_PRINT\.
+xcopy RichA777\sequencer_base_modified.stl             TO_PRINT\.
+
+REM HEN3DRIK parts
+xcopy HEN3DRIK\*.stl TO_PRINT\.
